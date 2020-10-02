8fcae198a92610f6226a18f04b654a513c7b5e60
Alexsandr <orleancity@gmail.com>
2020-10-02 11:46:23 +0300

    3.3. Разбор первой программы. [#375664]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexsandr Kaliuzhny");
+        System.out.println("08.01.86");
+    }
+}
