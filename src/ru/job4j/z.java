768f8befa17e737470ed069db6d7dffdafa1ddf6
Alexsandr <orleancity@gmail.com>
2020-10-11 17:18:17 +0300

    4.1. Аргументы [#375656]

---
+++ src/ru/job4j/z.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class z {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        z.plus(100, 500);
+        z.plus(4, 2);
+        z.plus(3, 5);
+    }
+}
