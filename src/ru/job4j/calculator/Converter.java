721f318ce838d9bf5fcf38c41d1d9d6fcbbd49b9
Alexsandr <orleancity@gmail.com>
2020-10-16 20:02:30 +0300

    0. Что такое тестирование [#375676]

--- src/ru/job4j/calculator/Converter.java
+++ src/ru/job4j/calculator/Converter.java
@@ -2,18 +2,28 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static double rubleToEuro(double value) {
-        double rsl = value / 70;
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
         return rsl;
     }
-    public static double rubleToDollar(double value) {
-        double rsl = value / 60;
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60;
         return rsl;
     }
     public static void main(String[] args) {
-        double euro = Converter.rubleToEuro(140);
-        double dollar = Converter.rubleToDollar(140);
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(140);
         System.out.println(euro);
         System.out.println(dollar);
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int in2 = 140;
+        int expected2 = 2;
+        int out2 = Converter.rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("140 rubles are 2. Test result : " + passed2);
     }
 }
--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -6,6 +6,7 @@ public class TrgArea {
         double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return rsl;
     }
+
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
