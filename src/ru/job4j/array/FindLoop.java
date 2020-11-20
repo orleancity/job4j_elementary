1eceda63e795710406ab10c02c937dd59cbdf1e0
Alexsandr <orleancity@gmail.com>
2020-11-20 18:39:32 +0300

    6.5. Поиск индекса в диапазоне. [#375619]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -2,8 +2,18 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int result = -1;
         for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int result = -1;
+        for (int index = start; index <= finish; index++) {
             if (data[index] == el) {
                 result = index;
                 break;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -32,4 +32,37 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind4() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 4;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind6() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 6;
+        int start = 0;
+        int finish = 2;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
