12fc45361ea55ae372aba191f7b938ba3061dc47
Alexsandr <orleancity@gmail.com>
2020-11-22 16:08:13 +0300

    6.6.2. Двухмерный массив. Циклы. [#375634]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -5,7 +5,7 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
             int t = data[i];
             if (data[i] > min) {
                 data[i] = data[index];
